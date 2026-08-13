e4ddd29c2ec91f0cbc552738053cfac74f2c375d
mukaddimalhasanorup-pixel <mukaddimalhasanorup@gmail.com>
2026-08-13 20:50:31 +0600

    Add Python calculator

---
+++ calculator.py
@@ -0,0 +1,27 @@
+n1 = float(input("Enter the 1st number: "))
+n2 = float(input("Enter the 2nd number: "))
+op =input(" Enter the arithmetic operator:")
+if op == '+':
+    result = n1 + n2
+elif op == '-':
+    result = n1 - n2
+elif op == '/':
+    if n2 == 0 :
+        result = "MATH ERROR: You cannot use zero here"
+    else:
+        result = n1 / n2
+elif op == '*':
+    result = n1 * n2
+elif op == '%':
+    if n2 == 0 :
+        result = "MATH ERROR: You cannot use zero here"
+    else:
+        result = n1 % n2
+elif op == '**':
+    if n1 == 0 and n2 <= 0:
+        result = "MATH ERROR: Invalid power"
+    else:
+        result = n1 ** n2
+else:
+    result = "MATH ERROR"
+print(f"Result = {result}")
